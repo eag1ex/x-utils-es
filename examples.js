916b0a48bda8bcb5f6774f807dc0e559986cd1b9
anonym101 <anon1@mailfence.com>
2021-05-01 10:27:40 +0000

    update package.json

--- examples.js
+++ examples.js
@@ -73,7 +73,7 @@ import {
      * `esm` > (default) and node support for with esnext,  // node -r esm examples 
      * `umd` > universal module/es2015 
     */
-} from './esm'
+} from './umd'
     // or {} = require('./umd') 
     
 loggerSetting('log', 'off') // disable all log()
